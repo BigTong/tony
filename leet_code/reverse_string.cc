d51850c8373488237ae13726ae9ef5412853e73c
BigTong <rengongzh@gmail.com>
2014-10-27 20:40:24 +0800

    Update reverse_string.cc

--- leet_code/reverse_string.cc
+++ leet_code/reverse_string.cc
@@ -1,5 +1,5 @@
 // Copyright 2014-10-15 The Tony Authors. All Rights Reserved.
-// Author: rentong_zhang@staff.easou.com (Rentong Zhang)
+// Author: rentongzh@gmail.com (Rentong Zhang)
 
 #include "tony/leet_code/reverse_string.h"
 
